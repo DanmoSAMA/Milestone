74b6672de2cc7dadb06f1d976e65331cb25c921f
DanmoSAMA <772523546@qq.com>
2022-02-07 09:03:21 +0800

    初始化前端文件夹

---
+++ fe/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script lang="ts">
+export default {
+  name: '',
+  components: [],
+  setup() {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
