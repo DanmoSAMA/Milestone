7370ef871049f8cab4702f9c467828ac705c50f5
DanmoSAMA <772523546@qq.com>
2022-02-08 20:25:24 +0800

    完成标签页轮廓

--- fe/src/pages/Tags/Tags.vue
+++ fe/src/pages/Tags/Tags.vue
@@ -1,7 +1,103 @@
 <template>
   <div class="tags">
     <SideBar />
-    <div>tags</div>
+    <div class="tags-main">
+      <h1 class="tags-main-title">标签</h1>
+      <div class="tags-main-cloud">
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+        <span class="tags-main-cloud-item">标签</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,4 +105,33 @@
 import SideBar from '../../components/SideBar/SideBar.vue';
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.tags {
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+
+  &-main {
+    width: 850px;
+    padding: 30px;
+
+    &-title {
+      font-size: 24px;
+      text-align: center;
+      margin: 30px 0;
+    }
+
+    &-cloud {
+      display: flex;
+      flex-wrap: wrap;
+      line-height: 40px;
+      justify-content: center;
+      align-items: center;
+
+      &-item {
+        margin: 10px;
+      }
+    }
+  }
+}
+</style>
