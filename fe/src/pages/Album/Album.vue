3c13bfddedbe1a022558657d8179608e71e3926b
DanmoSAMA <772523546@qq.com>
2022-02-08 21:48:47 +0800

    搭建简历页和相册页

--- fe/src/pages/Album/Album.vue
+++ fe/src/pages/Album/Album.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="album">
     <SideBar />
-    <div>album</div>
+    <div class="album-main">
+      <h1 class="album-main-title">
+        相册
+      </h1>
+      <div class="album-main-body">
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,5 +17,19 @@ import SideBar from '../../components/SideBar/SideBar.vue';
 </script>
 
 <style lang="scss">
+.album {
+  display: flex;
+  justify-content: center;
 
+  &-main {
+    width: 850px;
+    padding: 30px;
+
+    &-title {
+      font-size: 24px;
+      text-align: center;
+      margin: 30px 0;
+    }
+  }
+}
 </style>
--- fe/src/pages/Resume/Resume.vue
+++ fe/src/pages/Resume/Resume.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="resume">
     <SideBar />
-    <div>resume</div>
+    <div class="resume-main">
+      <h1 class="resume-main-title">
+        简历
+      </h1>
+      <div class="resume-main-body">
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,4 +16,20 @@
 import SideBar from '../../components/SideBar/SideBar.vue';
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.resume {
+  display: flex;
+  justify-content: center;
+
+  &-main {
+    width: 850px;
+    padding: 30px;
+
+    &-title {
+      font-size: 24px;
+      text-align: center;
+      margin: 30px 0;
+    }
+  }
+}
+</style>
