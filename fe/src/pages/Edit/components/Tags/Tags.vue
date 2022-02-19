1f1fa709a808e7a6f65d8b4ea12c24f113808cda
DanmoSAMA <772523546@qq.com>
2022-02-19 16:38:52 +0800

    将dropdown抽成组件

--- fe/src/pages/Edit/components/Tags/Tags.vue
+++ fe/src/pages/Edit/components/Tags/Tags.vue
@@ -13,35 +13,17 @@
         @click="setShowDropdown(true)"
         >+ 添加标签</span
       >
-      <div
-        :class="
-          showDropdown
-            ? 'app_wrapper-edit-body-tags-add-dropdown visible'
-            : 'app_wrapper-edit-body-tags-add-dropdown'
-        "
-      >
-        <div class="app_wrapper-edit-body-tags-add-dropdown-item">html</div>
-        <div class="app_wrapper-edit-body-tags-add-dropdown-item">css</div>
-        <div class="app_wrapper-edit-body-tags-add-dropdown-item">js</div>
-        <div class="app_wrapper-edit-body-tags-add-dropdown-item">ts</div>
-        <div class="app_wrapper-edit-body-tags-add-dropdown-item">vue</div>
-        <div class="app_wrapper-edit-body-tags-add-dropdown-item">react</div>
-        <div class="app_wrapper-edit-body-tags-add-dropdown-item">node</div>
-        <div class="app_wrapper-edit-body-tags-add-dropdown-item">koa</div>
-        <div class="app_wrapper-edit-body-tags-add-dropdown-item">os</div>
-        <div class="app_wrapper-edit-body-tags-add-dropdown-item">算法</div>
-        <div class="app_wrapper-edit-body-tags-add-dropdown-item">webpack</div>
-        <div class="app_wrapper-edit-body-tags-add-dropdown-item">脚手架</div>
-        <div class="app_wrapper-edit-body-tags-add-dropdown-item">浏览器</div>
-        <div class="app_wrapper-edit-body-tags-add-dropdown-item">其他</div>
-      </div>
+      <Dropdown :showDropdown="showDropdown" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useShowDropdown } from '../../../../hooks/useShowDropdown';
+import Dropdown from './components/Dropdown/Dropdown.vue'
+
 const { showDropdown, setShowDropdown } = useShowDropdown();
+
 </script>
 
 <style lang="scss">
@@ -73,42 +55,6 @@ const { showDropdown, setShowDropdown } = useShowDropdown();
     &-btn:hover {
       background-color: #6c757d;
       color: #fff;
-    }
-
-    &-dropdown {
-      width: 430px;
-      padding: 15px;
-      box-sizing: border-box;
-      background-color: #fff;
-      position: absolute;
-      top: 40px;
-      left: 0;
-      border: 1px solid #bbb;
-      flex-wrap: wrap;
-      align-items: flex-start;
-      display: none;
-      overflow: hidden;
-
-      &-item {
-        font-size: 14px;
-        height: 20px;
-        line-height: 20px;
-        padding: 4px 10px;
-        margin: 4px;
-        background-color: #e9f2ff;
-        border-radius: 3px;
-        color: #1f80ff;
-        transition: all 0.3s;
-        cursor: pointer;
-      }
-
-      &-item:hover {
-        background-color: #dee9ff;
-      }
-    }
-
-    &-dropdown.visible {
-      display: flex;
     }
   }
 }
---
+++ fe/src/pages/Edit/components/Tags/components/Dropdown/Dropdown.vue
@@ -0,0 +1,78 @@
+<template>
+  <div
+    :class="
+      props.showDropdown
+        ? 'app_wrapper-edit-body-tags-add-dropdown visible'
+        : 'app_wrapper-edit-body-tags-add-dropdown'
+    "
+  >
+    <div
+      class="app_wrapper-edit-body-tags-add-dropdown-item"
+      v-for="tag in tags"
+      :key="tag"
+    >
+      {{ tag }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  showDropdown: Boolean,
+});
+
+const tags = [
+  'html',
+  'css',
+  'js',
+  'ts',
+  'vue',
+  'react',
+  'node',
+  'koa',
+  'os',
+  '算法',
+  'webpack',
+  '脚手架',
+  '浏览器',
+  '其他',
+];
+</script>
+
+<style lang="scss">
+.app_wrapper-edit-body-tags-add-dropdown {
+  width: 430px;
+  padding: 15px;
+  box-sizing: border-box;
+  background-color: #fff;
+  position: absolute;
+  top: 40px;
+  left: 0;
+  border: 1px solid #bbb;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  display: none;
+  overflow: hidden;
+
+  &-item {
+    font-size: 14px;
+    height: 20px;
+    line-height: 20px;
+    padding: 4px 10px;
+    margin: 4px;
+    background-color: #e9f2ff;
+    border-radius: 3px;
+    color: #1f80ff;
+    transition: all 0.3s;
+    cursor: pointer;
+  }
+
+  &-item:hover {
+    background-color: #dee9ff;
+  }
+}
+
+.app_wrapper-edit-body-tags-add-dropdown.visible {
+  display: flex;
+}
+</style>
