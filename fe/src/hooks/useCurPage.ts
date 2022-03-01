c84cd43ef93a231a6807fc0eabbedf3297fcc54e
DanmoSAMA <772523546@qq.com>
2022-03-01 16:17:01 +0800

    修复刷新页面后，页面和侧栏不对应的bug

--- fe/src/hooks/useCurPage.ts
+++ fe/src/hooks/useCurPage.ts
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+import { ref } from 'vue';
 
 type curPageType = 'home' | 'tags' | 'search' | 'resume' | 'album' | 'edit';
 
@@ -15,4 +15,10 @@ type curPageType = 'home' | 'tags' | 'search' | 'resume' | 'album' | 'edit';
 //   }
 // }
 
-export const currentPage = ref<curPageType>('home');
+// 防止刷新页面后，页面组件和侧栏被选中的item不对应的bug
+const curLocation = window.location.href.split('/').at(-1) as string;
+const curType = (
+  curLocation.length <= 6 ? curLocation : curLocation.substring(0, 4)
+) as curPageType;
+
+export const currentPage = ref<curPageType>(curType);
