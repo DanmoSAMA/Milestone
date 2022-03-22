bf2e7fc1368e16bb815f145f6cfd7aba3262ebf2
DanmoSAMA <772523546@qq.com>
2022-03-22 17:22:45 +0800

    修改切换回首页时，仍然显示noPosts的bug

--- fe/src/pages/Posts/Posts.vue
+++ fe/src/pages/Posts/Posts.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="app_wrapper-posts">
-    <div :class="showContent ? 'app_wrapper-posts-inner' : 'app_wrapper-posts-inner hidden'">
+    <div
+      :class="
+        showContent
+          ? 'app_wrapper-posts-inner'
+          : 'app_wrapper-posts-inner hidden'
+      "
+    >
       <Post
         v-for="item in pagedPosts"
         :post="item"
@@ -50,7 +56,7 @@ curPageNum.value = getQuery().page
 tag.value = getQuery().tag ? (getQuery().tag as string) : '';
 kw.value = getQuery().kw ? (getQuery().kw as string) : '';
 
-function handlePosts() {
+function handleFilteredPosts() {
   // 根据query中的tag参数，筛选页面上的文章；如无tag，则不筛选
   filteredPosts.value =
     tag.value !== ''
@@ -64,22 +70,33 @@ function handlePosts() {
     ? filteredPosts.value.filter((item) => item.title.indexOf(kw.value) !== -1)
     : filteredPosts.value;
 
+  handlePagedPosts();
+}
+
+function handlePagedPosts() {
+  // 对筛选后的文章分页
   pagedPosts.value = filteredPosts.value.slice(
     curPageNum.value * eachPagePostNum,
     (curPageNum.value + 1) * eachPagePostNum
   );
 }
 
+function handleNoPosts() {
+  if (curPageNum.value === 0 && totalPageNum.value === 0) {
+    noPosts.value = true;
+    return true;
+  } else {
+    noPosts.value = false;
+    return false;
+  }
+}
+
 function handlePageNum() {
   totalPageNum.value = Math.ceil(filteredPosts.value.length / eachPagePostNum);
-
-  if (curPageNum.value !== Number.MAX_VALUE) {
-    if (curPageNum.value === 0 && totalPageNum.value === 0) {
-      noPosts.value = true;
-    } else if (
-      curPageNum.value < 0 ||
-      curPageNum.value > totalPageNum.value - 1
-    ) {
+  if (handleNoPosts()) {
+  } else if (curPageNum.value !== Number.MAX_VALUE) {
+    handleNoPosts();
+    if (curPageNum.value < 0 || curPageNum.value > totalPageNum.value - 1) {
       alert('页数错误');
       jump('/posts', { page: '0' });
     }
@@ -92,28 +109,27 @@ function handlePageNum() {
 // 虽然在sideBar请求了一次，但是为了发/删文章跳过来能直接看到更新，这里得再请求一次
 // 这里可以优化，设置某变量，通过watch使得sidebar那边重新请求数据...Todo
 postsStore.setPosts().then(() => {
-  handlePosts();
+  handleFilteredPosts();
   handlePageNum();
 });
 
 // 当curPageNum改变时，重新生成posts，避免页面空白
 watch(curPageNum, () => {
-  pagedPosts.value = filteredPosts.value.slice(
-    curPageNum.value * eachPagePostNum,
-    (curPageNum.value + 1) * eachPagePostNum
-  );
+  handlePagedPosts();
 });
 
 // 当tag改变时，重新生成posts
 watch(tag, () => {
-  handlePosts();
+  handleFilteredPosts();
   totalPageNum.value = Math.ceil(filteredPosts.value.length / eachPagePostNum);
+  handleNoPosts();
 });
 
 // 当kw改变时，重新生成posts
 watch(kw, () => {
-  handlePosts();
+  handleFilteredPosts();
   totalPageNum.value = Math.ceil(filteredPosts.value.length / eachPagePostNum);
+  handleNoPosts();
 });
 </script>
 
@@ -126,7 +142,7 @@ watch(kw, () => {
   min-height: 84vh;
 
   &-inner {
-    transition: all .8s;
+    transition: all 0.8s;
     padding-top: 10px;
   }
 
