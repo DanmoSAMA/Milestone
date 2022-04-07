ca70bbf91c5bae8d554be17f85f27a8cd9ca28bf
DanmoSAMA <772523546@qq.com>
2022-04-07 15:32:46 +0800

    gzip压缩

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
-
+import { svgBuilder } from './src/plugins/svgBuilder'
+import viteCompression from 'vite-plugin-compression'
 import vue from '@vitejs/plugin-vue'
 
-import { svgBuilder } from './src/plugins/svgBuilder'
-
 export default defineConfig({
-  plugins: [vue(), svgBuilder('./src/assets/fonts/svg/')],
+  plugins: [vue(), svgBuilder('./src/assets/fonts/svg/'), viteCompression()],
   // 存放环境变量
   envDir: './src/env'
 })
