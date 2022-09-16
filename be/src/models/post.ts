c9fa500352e4e14d3dbb38419faa0a00caa71223
DanmoSAMA <772523546@qq.com>
2022-09-17 00:04:14 +0800

    修改post.ts

--- be/src/models/post.ts
+++ be/src/models/post.ts
@@ -1,14 +1,12 @@
-import { Document, model, Model, Schema } from 'mongoose';
+import { Document, model, Model, Schema } from 'mongoose'
 
-import { Post as PostDef } from '../../../fe/shared/models/post';
+import { Post as PostDef } from '../../../fe/shared/models/post'
 
-const postSchema = new Schema(
-  {
-    title: String,
-    content: String,
-    tags: Array,
-  },
-);
+const postSchema = new Schema({
+  title: String,
+  content: String,
+  tags: Array
+})
 
 // 自动更改为名为 posts 的集合
-export const Post: Model<PostDef> = model("Post", postSchema);
+export const Post: Model<PostDef> = model('Post', postSchema)
